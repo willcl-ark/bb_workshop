3e43b83595e993826f6af0760c0392dec21ca4c9
willcl-ark <will8clark@gmail.com>
2019-05-20 16:07:35 +0100

    Removed unnecessary Kubernetes init.sh
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Grab LND 0.6.1-beta & extract
-wget https://github.com/lightningnetwork/lnd/releases/download/v0.6.1-beta/lnd-linux-amd64-v0.6.1-beta.tar.gz
-tar -C ~/ -xzf lnd-linux-amd64-v0.6.1-beta.tar.gz
-rm lnd-linux-amd64-v0.6.1-beta.tar.gz
-
-# Grab instructions from github
-wget https://raw.githubusercontent.com/willcl-ark/bb_workshop/master/basic_lnd_commands.py
