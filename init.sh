461625082ae4fbdd8835522bf2088a5eb9aebc38
willcl-ark <will8clark@gmail.com>
2019-05-20 14:09:07 +0100

    Kubernetes init script
    Signed-off-by: willcl-ark <will8clark@gmail.com>

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Grab LND 0.6.1-beta & extract
+wget https://github.com/lightningnetwork/lnd/releases/download/v0.6.1-beta/lnd-linux-amd64-v0.6.1-beta.tar.gz
+tar -C ~/ -xzf lnd-linux-amd64-v0.6.1-beta.tar.gz
+rm lnd-linux-amd64-v0.6.1-beta.tar.gz
+
+# Grab instructions from github
+wget https://github.com/lightningnetwork/lnd/releases/download/v0.6.1-beta/lnd-linux-amd64-v0.6.1-beta.tar.gz
